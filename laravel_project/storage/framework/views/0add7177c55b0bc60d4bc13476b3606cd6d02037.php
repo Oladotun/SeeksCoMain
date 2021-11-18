8162af8d84ebabc773f8e26ae254e84319787031
root <oladotunopasina@gmail.com>
2021-11-18 01:08:14 +0000

    updated first page screen

--- laravel_project/storage/framework/views/0add7177c55b0bc60d4bc13476b3606cd6d02037.php
+++ laravel_project/storage/framework/views/0add7177c55b0bc60d4bc13476b3606cd6d02037.php
@@ -1,6 +1,6 @@
 <ul class="list-group w-50 mx-auto col-lg-12 text-center " style="color: <?php echo e($site_innerpage_header_title_font_color); ?>;">
-    <li class="list-group-item list-group-item-success"><strong>  Find Nearby Businesses and Services </strong> </li>
-    <li class="list-group-item list-group-item-primary"><strong>  Use Local Discounts </strong></li>
-    <li class="list-group-item list-group-item-warning"><strong>  Explore In-Town Jobs </strong></li>
-    <li class="list-group-item list-group-item-info"><strong>  Create Lit Moments With Friends </strong></li>
+    <li class="list-group-item list-group-item" style="background:teal; ";><strong>  Find Nearby Businesses and Services </strong> </li>
+    <li class="list-group-item list-group-item" style="background: #64A8F0"><strong>  Use Local Discounts </strong></li>
+    <li class="list-group-item list-group-item" style="background: #b8860b;"><strong>  Explore In-Town Jobs </strong></li>
+    <li class="list-group-item list-group-item-danger"><strong>  Create Lit Moments With Friends </strong></li>
 </ul><?php /**PATH /var/www/seekscodirectory/laravel_project/theme_views/frontend_views/lduruo10_dh_frontend_city_path/partials/first-page-words.blade.php ENDPATH**/ ?>
--- laravel_project/theme_views/frontend_views/lduruo10_dh_frontend_city_path/partials/first-page-words.blade.php
+++ laravel_project/theme_views/frontend_views/lduruo10_dh_frontend_city_path/partials/first-page-words.blade.php
@@ -1,6 +1,6 @@
 <ul class="list-group w-50 mx-auto col-lg-12 text-center " style="color: {{ $site_innerpage_header_title_font_color }};">
-    <li class="list-group-item list-group-item-success"><strong>  Find Nearby Businesses and Services </strong> </li>
-    <li class="list-group-item list-group-item-primary"><strong>  Use Local Discounts </strong></li>
-    <li class="list-group-item list-group-item-warning"><strong>  Explore In-Town Jobs </strong></li>
-    <li class="list-group-item list-group-item-info"><strong>  Create Lit Moments With Friends </strong></li>
+    <li class="list-group-item list-group-item" style="background:teal; ";><strong>  Find Nearby Businesses and Services </strong> </li>
+    <li class="list-group-item list-group-item" style="background: #64A8F0"><strong>  Use Local Discounts </strong></li>
+    <li class="list-group-item list-group-item" style="background: #b8860b;"><strong>  Explore In-Town Jobs </strong></li>
+    <li class="list-group-item list-group-item-danger"><strong>  Create Lit Moments With Friends </strong></li>
 </ul>
